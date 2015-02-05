d62860968e3564e081fc7606aa7c129e7a5d3806
Josue <josuebm@hotmail.com>
2015-02-05 21:13:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/josu/dibujos/Principal.java
@@ -0,0 +1,137 @@
+package com.example.josu.dibujos;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class Principal extends Activity{
+
+    private MenuItem menuItem, menuItemGrosor;
+    private Vista vista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        vista = new Vista(this);
+        setContentView(vista);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        menuItem = menu.findItem(R.id.action_figura);
+        menuItemGrosor = menu.findItem(R.id.action_grosor);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_color) {
+            vista.cambioColor(this, vista);
+            return true;
+        }else if(id == R.id.action_grosor){
+            vista.cambioGrosor(this, vista);
+            return true;
+        }else if(id == R.id.action_linea){
+            menuItem.setIcon(R.drawable.linea);
+            vista.setFigura("linea");
+            menuItemGrosor.setVisible(true);
+        }else if(id == R.id.action_cuadrado){
+            menuItem.setIcon(R.drawable.square);
+            vista.setFigura("cuadrado");
+            menuItemGrosor.setVisible(true);
+        }else if(id == R.id.action_circulo){
+            menuItem.setIcon(R.drawable.circle);
+            vista.setFigura("circulo");
+            menuItemGrosor.setVisible(true);
+        }else if(id == R.id.action_lapiz){
+            menuItem.setIcon(R.drawable.lapiz);
+            vista.setFigura("lapiz");
+            menuItemGrosor.setVisible(true);
+        }else if(id == R.id.action_cuadrado_relleno) {
+            menuItem.setIcon(R.drawable.square_relleno);
+            vista.setFigura("cuadrado relleno");
+            menuItemGrosor.setVisible(false);
+        }else if(id == R.id.action_circulo_relleno) {
+            menuItem.setIcon(R.drawable.circle_relleno);
+            vista.setFigura("circulo relleno");
+            menuItemGrosor.setVisible(false);
+        }else if(id == R.id.action_goma) {
+            vista = new Vista(this);
+            setContentView(vista);
+            menuItem.setIcon(R.drawable.lapiz);
+            vista.setFigura("lapiz");
+            menuItemGrosor.setVisible(true);
+        }else if(id == R.id.action_guardar) {
+            try {
+                guardarFoto(vista.getDibujo());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tostada(String mensaje){
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    public void guardarFoto(final Bitmap foto) throws IOException {
+        final EditText input = new EditText(this);
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.nombre_guardar))
+                .setView(input)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        FileOutputStream fos = null;
+                        String nombre = input.getText().toString() + ".jpg";
+                        File archivo = null;
+
+                        if (espacioSuficiente(getExternalFilesDir(Environment.DIRECTORY_DCIM))) {
+                            archivo = new File(getExternalFilesDir(Environment.DIRECTORY_DCIM), nombre);
+                            if (!archivo.exists()) {
+                                try {
+                                    fos = new FileOutputStream(archivo);
+                                    fos.flush();
+                                    foto.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            } else
+                                tostada(getResources().getString(R.string.nombre_usado));
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .show();
+
+    }
+
+    public boolean espacioSuficiente(File f) {
+        double eTotal, eDisponible, porcentaje;
+        eTotal = (double) f.getTotalSpace();
+        eDisponible = (double) f.getFreeSpace();
+        porcentaje = (eDisponible / eTotal) * 100;
+        return porcentaje > 10;
+    }
+}
---
+++ app/src/main/java/com/example/josu/dibujos/Vista.java
@@ -0,0 +1,190 @@
+package com.example.josu.dibujos;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SeekBar;
+
+/**
+ * Created by Josué on 05/02/2015.
+ */
+public class Vista extends View implements SeekBar.OnSeekBarChangeListener, ColorPickerDialog.OnColorSelectedListener{
+
+    private Paint pincel;
+    private Bitmap mapaDeBits;
+    private float x0=-1, y0=-1, xi=-1, yi=-1;
+    private Canvas lienzoFondo;
+    private String figura;
+    private Path rectaPoligonal = new Path();
+    private double radio =0;
+    private int grosor = 1;
+
+    public Vista(Context context) {
+        super(context);
+        pincel = new Paint();
+        pincel.setColor(Color.BLACK);
+        pincel.setAntiAlias(true);
+        figura = "lapiz";
+    }
+
+    @Override
+    protected void onDraw(Canvas lienzo) {
+        super.onDraw(lienzo);
+        lienzo.drawColor(Color.WHITE);
+        //for(Recta r: rectas)
+        //    lienzo.drawLine(r.x0, r.y0, r.xi, r.yi, pincel);
+        lienzo.drawBitmap(mapaDeBits, 0, 0, null);
+        if(figura.equals("linea"))
+            lienzo.drawLine(x0, y0, xi, yi, pincel);
+        else if(figura.equals("lapiz")){
+            lienzo.drawPath(rectaPoligonal, pincel);
+            pincel.setStyle(Paint.Style.STROKE);
+        }else if (figura.equals("cuadrado")){
+            float xorigen = Math.min(x0, xi);
+            float xdestino = Math.max(x0, xi);
+            float yorigen = Math.min(y0, yi);
+            float ydestino = Math.max(y0, yi);
+            pincel.setStyle(Paint.Style.STROKE);
+            lienzo.drawRect(xorigen, yorigen, xdestino, ydestino, pincel);
+        }else if(figura.equals("circulo")){
+            lienzo.drawCircle(x0, y0, (float)radio, pincel);
+            pincel.setStyle(Paint.Style.STROKE);
+        }else if (figura.equals("cuadrado relleno")) {
+            float xorigen = Math.min(x0, xi);
+            float xdestino = Math.max(x0, xi);
+            float yorigen = Math.min(y0, yi);
+            float ydestino = Math.max(y0, yi);
+            pincel.setStyle(Paint.Style.FILL);
+            lienzo.drawRect(xorigen, yorigen, xdestino, ydestino, pincel);
+        }else if(figura.equals("circulo relleno")) {
+            lienzo.drawCircle(x0, y0, (float) radio, pincel);
+            pincel.setStyle(Paint.Style.FILL);
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:{
+                    x0 = x;
+                    y0 = y;
+                if(figura.equals("lapiz")){
+                    rectaPoligonal.reset();
+                    rectaPoligonal.moveTo(x0, y0);
+                }
+            }
+            break;
+            case MotionEvent.ACTION_MOVE:{
+                xi = x;
+                yi = y;
+                if(figura.equals("lapiz"))
+                    rectaPoligonal.quadTo(xi, yi, (x + xi) / 2, (y + yi)/2);
+                else if(figura.startsWith("circulo"))
+                    radio = Math.sqrt(Math.pow(xi - x0, 2) + Math.pow(yi - y0, 2));
+                invalidate();
+            }
+            break;
+            case MotionEvent.ACTION_UP:{
+                xi = x;
+                yi = y;
+                if(figura.equals("linea"))
+                    lienzoFondo.drawLine(x0, y0, xi, yi, pincel);
+                else if(figura.equals("lapiz"))
+                    lienzoFondo.drawPath(rectaPoligonal, pincel);
+                else if (figura.startsWith("cuadrado")){
+                    if(xi < x0 || yi < y0)
+                        lienzoFondo.drawRect(xi, yi, x0, y0, pincel);
+                    else
+                        lienzoFondo.drawRect(x0, y0, xi, yi, pincel);
+                }else if (figura.startsWith("circulo")){
+                    radio = Math.sqrt(Math.pow(xi - x0, 2) + Math.pow(yi - y0, 2));
+                    lienzoFondo.drawCircle(x0, y0, (float)radio, pincel);
+                }
+                invalidate();
+                radio = 0;
+                x0=y0=xi=yi=-1;
+            }
+            break;
+        }
+        return true;
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mapaDeBits = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mapaDeBits.eraseColor(Color.WHITE);
+        lienzoFondo = new Canvas(mapaDeBits);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        grosor = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onColorSelected(int color) {
+        pincel.setColor(color);
+    }
+
+
+    class Recta{
+        public float x0, y0, xi, yi;
+
+        Recta(float x0, float y0, float xi, float yi) {
+            this.x0 = x0;
+            this.y0 = y0;
+            this.xi = xi;
+            this.yi = yi;
+        }
+    }
+
+    public void cambioColor(Context contexto, ColorPickerDialog.OnColorSelectedListener listener){
+        ColorPickerDialog colorPickerDialog = new ColorPickerDialog(contexto, pincel.getColor(), listener);
+        colorPickerDialog.show();
+    }
+
+    public void cambioGrosor(Context contexto, SeekBar.OnSeekBarChangeListener listener){
+        SeekBar barra = new SeekBar(contexto);
+        barra.setOnSeekBarChangeListener(listener);
+        barra.setProgress((int)pincel.getStrokeWidth());
+        barra.setMax(20);
+        new AlertDialog.Builder(contexto)
+                .setTitle(getResources().getString(R.string.grosor_pincel))
+                .setView(barra)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        pincel.setStrokeWidth(grosor);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .show();
+    }
+
+    public void setFigura(String figura){
+        this.figura = figura;
+    }
+
+    public Bitmap getDibujo(){
+        return mapaDeBits;
+    }
+}
